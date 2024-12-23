c83a77901082fd163cb1e4611d707694c4585316
zishanmeh <zishanmehedihasan2@gmail.com>
2024-12-23 22:16:47 +0000

    Added sort by price from server side

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -1,38 +1,73 @@
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
 import { Helmet } from "react-helmet";
 import { Link, useLoaderData } from "react-router-dom";
 
 const Rooms = () => {
-  const rooms = useLoaderData();
+  const [rooms, setRooms] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3000/").then((res) => setRooms(res.data));
+  }, []);
+  const handleShortByPrice = () => {
+    axios
+      .get("http://localhost:3000/roomsSortByPrice")
+      .then((res) => setRooms(res.data));
+  };
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-between items-stretch gap-5">
+    <div>
       <Helmet>
         <meta charSet="utf-8" />
         <title>All rooms</title>
       </Helmet>
-      {rooms.map((room) => (
-        <Link
-          className="card bg-base-100 shadow-xl cursor-pointer hover:scale-105 duration-700"
-          key={room._id}
-          to={`/room/${room._id}`}
-        >
-          <figure>
-            <img src={room.image} alt={`Image of ${room.type}`} />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">
-              {room.type}
-              <div className="badge badge-secondary">
-                {room.availability ? "Available" : "Unavailable"}
-              </div>
-            </h2>
-            <p className="text-left">{room.description}</p>
-            <div className="card-actions justify-end">
-              <div className="badge badge-outline">{room.capacity} persons</div>
-              <div className="badge badge-outline">{room.view} view</div>
-            </div>
-          </div>
+      <div className="flex justify-between items-center mb-10">
+        <Link className="btn btn-neutral" to={-1}>
+          Back
         </Link>
-      ))}
+        <button
+          className="btn btn-neutral btn-outline"
+          onClick={handleShortByPrice}
+        >
+          Short by price
+        </button>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-between items-stretch gap-5">
+        {rooms.map((room) => (
+          <Link
+            className="card bg-base-100 shadow-xl cursor-pointer hover:scale-105 duration-700"
+            key={room._id}
+            to={`/room/${room._id}`}
+          >
+            <figure>
+              <img src={room.image} alt={`Image of ${room.type}`} />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                {room.type}
+                <div
+                  className={`badge badge-secondary ${
+                    room.availability
+                      ? "bg-green-200 text-green-700"
+                      : "bg-red-200 text-red-700"
+                  } border-none`}
+                >
+                  {room.availability ? "Available" : "Unavailable"}
+                </div>
+              </h2>
+              <p className="text-left">{room.description}</p>
+              <div className="card-actions justify-end items-center">
+                <p className="font-bold badge badge-neutral">
+                  Price: {room.price}$
+                </p>
+                <div className="badge badge-outline">
+                  {room.capacity} persons
+                </div>
+                <div className="badge badge-outline">{room.view} view</div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
